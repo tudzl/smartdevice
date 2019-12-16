cd268bc6905610a401a400f790738d1acadc7fc3
Zell <56329974+tudzl@users.noreply.github.com>
2019-12-16 14:30:51 +0800

    bug fixed , lux limit

--- Sensors lib/libraries/MAX44009_library/src/MAX44009.cpp
+++ Sensors lib/libraries/MAX44009_library/src/MAX44009.cpp
@@ -15,8 +15,10 @@ int MAX44009::begin()
 bool MAX44009::begin2(uint8_t addr) 
 {
 	Wire.beginTransmission(addr);
-	Wire.write(0x02);
-	Wire.write(0x40);
+	if (MAX44009::manual_mode) {
+		Wire.write(MAX44009_CONFIGURATION);
+		Wire.write(MAX44009_manualmode);
+	}
 	int res = Wire.endTransmission();  
 	if (0 == res)
 		return true;
@@ -25,6 +27,35 @@ bool MAX44009::begin2(uint8_t addr)
 }
 
 float MAX44009::get_lux(void)
+{
+	unsigned int data[2];
+	Wire.beginTransmission(MAX_ADDR);
+	Wire.write(0x03);
+	Wire.endTransmission();
+ 
+	// Request 2 bytes of data
+	Wire.requestFrom(MAX_ADDR, 2);
+ 
+	// Read 2 bytes of data luminance msb, luminance lsb
+	if (Wire.available() == 2)
+	{
+    	data[0] = Wire.read();  // high byte
+    	data[1] = Wire.read(); // low byte
+	}
+ 
+	// Convert the data to lux
+	int exponent = (data[0] & 0xF0) >> 4;
+	int mantissa = ((data[0] & 0x0F) << 4) | (data[1] & 0x0F);
+	
+	//float luminance = pow(2, exponent) * mantissa * 0.045;
+	float luminance = (float)(((0x00000001 << exponent) * (float)mantissa) * 0.045);
+  
+	return luminance; 
+}
+
+/*
+//read both high and low lux reg test
+float MAX44009::read_lux(void)
 {
 	unsigned int data[2];
 	Wire.beginTransmission(MAX_ADDR);
@@ -50,3 +81,4 @@ float MAX44009::get_lux(void)
   
 	return luminance; 
 }
+*/
--- Sensors lib/libraries/MAX44009_library/src/MAX44009.h
+++ Sensors lib/libraries/MAX44009_library/src/MAX44009.h
@@ -5,6 +5,15 @@
 
 #define MAX_ADDR 0x4B
 
+//Device Reg add
+#define MAX44009_CONFIGURATION 0x02
+
+//Device Reg values
+
+#define MAX44009_Autolmode 0x00
+#define MAX44009_manualmode 0x40
+#define MAX44009_CDR 0x08  // 1/8 current ratio
+#define MAX44009_TIM 0x03  // preferred mode for high brightness mode
 /*
 Lux = (2(exponent) x mantissa) x 0.045
 
@@ -16,9 +25,9 @@ in units of lux.
 class MAX44009 {
     public:
 		MAX44009();
-		
+		bool manual_mode = false; // true for 0.045 LUX high resolution low intensity mode, MAX lux is limited to 2938 LUX
 		int begin();
-		bool begin2(uint8_t addr);        
+		bool begin2(uint8_t addr);       //by zl  
 		float get_lux(void);
 		
 };
