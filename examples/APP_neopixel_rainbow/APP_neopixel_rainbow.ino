5a5933b40310f7db8e5d82d1575048cdaf2437f6
Zell <56329974+tudzl@users.noreply.github.com>
2020-03-17 23:19:26 +0800

    Add files via upload

--- examples/APP_neopixel_rainbow/APP_neopixel_rainbow.ino
+++ examples/APP_neopixel_rainbow/APP_neopixel_rainbow.ino
@@ -1,9 +1,10 @@
 /*  M5stack fire rainbow pixels ws2812B demo app
- *   modified by zell 
- *   V 1.1  17.03.2020
+     modified by zell
+     V 1.2  17.03.2020
+     task list, need to save NUM_LEDS to flash and readback
     Please install FastLED library first.
     In arduino library manage search FastLED
- */
+*/
 #include <M5Stack.h>
 #include "FastLED.h"
 
@@ -14,6 +15,7 @@ POWER m5_power;
 #define Neopixel_PIN    26 //fire GROVE  port B
 #define NUM_LEDS    64
 
+uint8_t Pixel_brightness = 10 ;
 uint8_t active_leds = 0;
 CRGB leds[NUM_LEDS];
 uint8_t gHue = 0;
@@ -22,31 +24,47 @@ static TaskHandle_t userTaskHandle = 0;
 
 unsigned long run_cnt = 0;
 
-//increase num of leds 
+//increase num of leds
 void buttonC_wasPressed(void) {
   //M5.Speaker.beep();  // too laud
 
   Serial.println("BtnC was Pressed\r\n");
   active_leds++;
-   if ( active_leds> NUM_LEDS)
-    active_leds =NUM_LEDS ;
+  if ( active_leds > NUM_LEDS)
+    active_leds = NUM_LEDS ;
   Serial.printf("active_leds: %d\r\n", active_leds);
-
+  //FastLED.addLeds<WS2812B, Neopixel_PIN, GRB>(leds, active_leds).setCorrection(TypicalLEDStrip);
 
 }
 
-//decrease num of leds 
+//decrease num of leds
 void buttonA_wasPressed(void) {
   //M5.Speaker.beep();  // too laud
 
   Serial.println("BtnA was Pressed\r\n");
   active_leds--;
-   if ( active_leds< 0)
+  if ( active_leds < 0)
     active_leds = 0 ;
   Serial.printf("active_leds: %d\r\n", active_leds);
 
+  //FastLED.addLeds<WS2812B, Neopixel_PIN, GRB>(leds, active_leds).setCorrection(TypicalLEDStrip);
+}
+
+
+//decrease num of leds
+void buttonB_wasPressed(void) {
+  //M5.Speaker.beep();  // too laud
+
+  Serial.println("BtnB was Pressed\r\n");
+  Pixel_brightness += 10;
+  if ( Pixel_brightness > 255)
+    Pixel_brightness = 10 ;
+  Serial.printf("Pixel_brightness: %d\r\n", Pixel_brightness);
+
+  FastLED.setBrightness(Pixel_brightness);
 
 }
+
 
 
 
@@ -63,7 +81,7 @@ void setup() {
 
 
 
-  M5.Lcd.setBrightness(50); 
+  M5.Lcd.setBrightness(50);
   M5.Lcd.clear(BLACK);
   M5.Lcd.setTextColor(YELLOW); M5.Lcd.setTextSize(2); M5.Lcd.setCursor(40, 0);
   M5.Lcd.println(" FastLED Neopixel demo");
@@ -72,13 +90,13 @@ void setup() {
   M5.Lcd.setTextColor(RED);
   M5.Lcd.println("Display rainbow effect");
   M5.Lcd.setTextColor(GREEN);
-  active_leds =NUM_LEDS;
+  active_leds = NUM_LEDS;
   M5.Lcd.setCursor(10, 100);
-  M5.Lcd.printf("LED Num: %d",active_leds );
-   
-  
+  M5.Lcd.printf("LED Num: %d", active_leds );
+
+
   // Neopixel initialization, 2811,2812,etc....
-  FastLED.addLeds<WS2812B,Neopixel_PIN,GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812B, Neopixel_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(10);
   xTaskCreatePinnedToCore(FastLEDshowTask, "FastLEDshowTask", 2048, NULL, 2, NULL, 1);
 }
@@ -86,7 +104,7 @@ void setup() {
 void loop()
 {
   run_cnt++;
- 
+
   M5.update(); // This function reads The State of Button A and B and C.
 
   //change num
@@ -102,47 +120,58 @@ void loop()
     //delay(100);
   }
 
+  //change gain
+  if (M5.BtnB.wasPressed() ) {
+    buttonB_wasPressed();
 
-  if ( run_cnt % 10 ==0){
-
-  active_leds =NUM_LEDS;
-  M5.Lcd.setCursor(10, 100);
-  M5.Lcd.printf("LED Num: %d",active_leds );
-
-  int Akku_level = m5_power.getBatteryLevel();
-  M5.Lcd.setCursor(0, 220);
-  M5.Lcd.setTextSize(2); //size 2 to 8
-  M5.Lcd.setTextColor(ORANGE, BLACK);
-  M5.Lcd.printf("Akku: %3d%%   Run:%d", Akku_level, run_cnt);
+    //delay(100);
   }
-  
+
+
+  if ( run_cnt % 100 == 0) {
+    //FastLED.addLeds<WS2812B, Neopixel_PIN, GRB>(leds, active_leds).setCorrection(TypicalLEDStrip);
+    //active_leds =NUM_LEDS;
+    M5.Lcd.setCursor(10, 100);
+    M5.Lcd.printf("LED Num: %d", active_leds );
+    M5.Lcd.setTextColor(YELLOW, BLACK);
+     M5.Lcd.setCursor(10, 120);
+    M5.Lcd.printf("Brightness: %d", Pixel_brightness );
+    
+    int Akku_level = m5_power.getBatteryLevel();
+    M5.Lcd.setCursor(0, 220);
+    M5.Lcd.setTextSize(2); //size 2 to 8
+    M5.Lcd.setTextColor(ORANGE, BLACK);
+    M5.Lcd.printf("Akku: %3d%%   Run:%d", Akku_level, run_cnt);
+  }
+
 }
 
 void FastLEDshowESP32()
 {
-    if (userTaskHandle == 0) {
-        userTaskHandle = xTaskGetCurrentTaskHandle();
-        xTaskNotifyGive(FastLEDshowTaskHandle);
-        const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 200 );
-        ulTaskNotifyTake(pdTRUE, xMaxBlockTime);
-        userTaskHandle = 0;
-    }
+  if (userTaskHandle == 0) {
+    userTaskHandle = xTaskGetCurrentTaskHandle();
+    xTaskNotifyGive(FastLEDshowTaskHandle);
+    const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 200 );
+    ulTaskNotifyTake(pdTRUE, xMaxBlockTime);
+    userTaskHandle = 0;
+  }
 }
 
 void FastLEDshowTask(void *pvParameters)
 {
-    for(;;) {
-        fill_rainbow(leds, NUM_LEDS, gHue, 7);// rainbow effect
-        FastLED.show();// must be executed for neopixel becoming effective
-        EVERY_N_MILLISECONDS( 20 ) { gHue++; }
+  for (;;) {
+    fill_rainbow(leds, NUM_LEDS, gHue, 8);// rainbow effect
+    FastLED.show();// must be executed for neopixel becoming effective
+    EVERY_N_MILLISECONDS( 20 ) {
+      gHue++;
     }
+  }
 }
 
 /*
- /// fill_rainbow - fill a range of LEDs with a rainbow of colors, at
-///                full saturation and full value (brightness)
-void fill_rainbow( struct CRGB * pFirstLED, int numToFill,
+  /// fill_rainbow - fill a range of LEDs with a rainbow of colors, at
+  ///                full saturation and full value (brightness)
+  void fill_rainbow( struct CRGB * pFirstLED, int numToFill,
                    uint8_t initialhue,
                    uint8_t deltahue = 5);
-
 */
